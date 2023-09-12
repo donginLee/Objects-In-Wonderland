231b9025a4cd9c26a51b578420f8f7cb6a612621
Harper <55528388+donginLee@users.noreply.github.com>
2023-09-12 11:48:58 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -140,7 +140,7 @@ function draw() {
   directionalLight(255, 255, 255, 1, 0, -1);
   // lights();
   let level = mic.getLevel();
-  vel = level * 50;
+  vel = level * 10;
     console.log(vel);
   print(vel);
   
